2bc8eb736ec058e3cdb432b12f59a15531aaa964
BruvLio <lionel.mbayu@gmail.com>
2023-06-16 15:06:31 -0500

    More components

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,27 @@
+import image1 from '../../assets/Profile2.jpg';
+import image2 from '../../assets/Profile2.jpg';
+import image3 from '../../assets/Profile2.jpg';
+
+export const Data = [
+	{
+		id: 1,
+		Image: image1,
+		title: 'Latoya Mbayu',
+		description:
+			'A really good job, all aspects of the project were followed step by step and with good results.',
+	},
+	{
+		id: 2,
+		Image: image2,
+		title: 'Larry Mbayu',
+		description:
+			'A really good job, all aspects of the project were followed step by step and with good results.',
+	},
+	{
+		id: 3,
+		Image: image3,
+		title: 'Enoka Jaona',
+		description:
+			'A really good job, all aspects of the project were followed step by step and with good results.',
+	},
+];
